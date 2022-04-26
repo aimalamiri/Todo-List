3eedbd04ddf76418b57abbcd4b50e420c17be3ee
Aimal Amiri <meaimal@hotmail.com>
2022-04-27 02:11:55 +0430

    Create tasks object, and a loot to insert them into DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './scss/index.scss';
+import appendChild from './js/utilities';
+
+const tasks = [
+  {
+    description: 'Build todo list',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Ask for the code review',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Implement changes',
+    completed: false,
+    index: 2,
+  },
+];
+
+window.onload = () => {
+  for (let i = 0; i < tasks.length; i += 1) {
+    appendChild(`<li>${tasks[i].description}</li>`, '#list');
+  }
+};
