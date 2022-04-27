9760e5fd37f3d7ecd7045d897e7443392a734ea4
Aimal Amiri <meaimal@hotmail.com>
2022-04-27 06:41:31 +0430

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './scss/index.scss';
-import appendChild from './js/utilities';
+import appendChild from './js/utilities.js';
 
 const tasks = [
   {
@@ -20,19 +20,6 @@ const tasks = [
 ];
 
 window.onload = () => {
-  appendChild(
-    `
-    <li class="p-1 text-gray border-b-1 flex justify-between items-center">
-      <h3>Today's Todo</h3>
-      <a href="#" class="flex items-center"><span class="icon icon-reload" /></a>
-    </li> 
-    <li class="p-1 text-gray border-b-1 flex justify-between items-center">
-      <input type="text" class="task-input" placeholder="Add to your list...">
-      <a href="#" class="flex items-center"><span class="icon icon-enter" /></a>
-    </li> 
-  `,
-    '#list'
-  );
   for (let i = 0; i < tasks.length; i += 1) {
     appendChild(
       `
@@ -46,15 +33,7 @@ window.onload = () => {
       </div>
       <a href="#" class="flex items-center"><span class="icon icon-dots" /></a>
     </li>`,
-      '#list'
+      '#list',
     );
   }
-  appendChild(
-    `
-    <li class="p-1 text-gray flex justify-center">
-      <a href="#">Clear all completed</a>
-    </li> 
-  `,
-    '#list'
-  );
 };
