79865b0190926b1b49809b54e61b2c1e0b0f3150
Aimal Amiri <meaimal@hotmail.com>
2022-04-27 05:40:26 +0430

    Design tasks layout section with icons, and checkbox

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import appendChild from './js/utilities';
 const tasks = [
   {
     description: 'Build todo list',
-    completed: false,
+    completed: true,
     index: 0,
   },
   {
     description: 'Ask for the code review',
-    completed: false,
+    completed: true,
     index: 1,
   },
   {
@@ -20,12 +20,41 @@ const tasks = [
 ];
 
 window.onload = () => {
+  appendChild(
+    `
+    <li class="p-1 text-gray border-b-1 flex justify-between items-center">
+      <h3>Today's Todo</h3>
+      <a href="#" class="flex items-center"><span class="icon icon-reload" /></a>
+    </li> 
+    <li class="p-1 text-gray border-b-1 flex justify-between items-center">
+      <input type="text" class="task-input" placeholder="Add to your list...">
+      <a href="#" class="flex items-center"><span class="icon icon-enter" /></a>
+    </li> 
+  `,
+    '#list'
+  );
   for (let i = 0; i < tasks.length; i += 1) {
-    appendChild(`
-    <li class="p-1 text-gray border-b-1">
-      <input type="checkbox" class="mr-0.5" />
-      ${tasks[i].description}
+    appendChild(
+      `
+    <li class="p-1 text-gray border-b-1 flex justify-between items-center">
+      <div>
+        <label class="control control-checkbox" for="task-${i}">
+          ${tasks[i].description}
+          <input type="checkbox" id="task-${i}" ${tasks[i].completed ? 'checked' : ''} />
+          <div class="control_indicator"></div>
+        </label>
+      </div>
+      <a href="#" class="flex items-center"><span class="icon icon-dots" /></a>
     </li>`,
-     '#list');
+      '#list'
+    );
   }
+  appendChild(
+    `
+    <li class="p-1 text-gray flex justify-center">
+      <a href="#">Clear all completed</a>
+    </li> 
+  `,
+    '#list'
+  );
 };
