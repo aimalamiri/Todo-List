16fe8985e02be5ac7c2c4f5acf459c2ada19afa2
Aimal Amiri <meaimal@hotmail.com>
2022-05-07 23:27:22 +0430

    Updates

--- src/js/Task.js
+++
@@ -1,8 +0,0 @@
-export default class Task {
-  constructor(description, index, complete = false) {
-    this.id = `id-${Math.round(Math.random() * 1000000)}`;
-    this.description = description;
-    this.complete = complete;
-    this.index = index;
-  }
-}
